242f941bf604d788772c24d78fc229b18e2cd29b
Andreas Philippi <18646892+andreasphil@users.noreply.github.com>
2025-08-09 20:14:48 +0200

    refactor: simplify scripts

--- dist/useAsyncTask.js
+++ dist/useAsyncTask.js
@@ -1,2 +1,56 @@
-import{computed as i,ref as t}from"vue";function y(s,u){let a=t(),r=u?.isLoading??t(!1),n=u?.error??t(void 0),d=i(()=>!!n.value);return{run:async(...f)=>{r.value=!0,n.value=void 0;try{let e=await s(...f);return a.value=e,[e,void 0]}catch(e){return n.value=e,[void 0,e]}finally{r.value=!1}},data:a,isLoading:r,error:n,hasError:d}}export{y as useAsyncTask};
-//# sourceMappingURL=useAsyncTask.js.map
+// src/useAsyncTask.ts
+import { computed, ref } from "vue";
+function useAsyncTask(fetcher, shared) {
+  const data = ref();
+  const isLoading = shared?.isLoading ?? ref(false);
+  const error = shared?.error ?? ref(void 0);
+  const hasError = computed(() => !!error.value);
+  const run = async (...args) => {
+    isLoading.value = true;
+    error.value = void 0;
+    try {
+      const result = await fetcher(...args);
+      data.value = result;
+      return [result, void 0];
+    } catch (e) {
+      error.value = e;
+      return [void 0, e];
+    } finally {
+      isLoading.value = false;
+    }
+  };
+  return {
+    /**
+     * Executes the task and updates all the state properties. Returns a tuple
+     * of the shape `[data, error]` that contains the result of the task. If
+     * the task succeeded, `data` will be set to the return value (if one
+     * exists), `error` will be undefined. If the task threw, `data` will be
+     * undefined and `error` will be set to the value that was thrown. Note
+     * that `run` itself never throws, so you don't need to catch anything.
+     *
+     * Example:
+     *
+     * ```
+     * const { run } = useAsyncTask(myTask);
+     * const [data, error] = await run();
+     * if (error) {
+     *   // Handle error here
+     * } else {
+     *   // Do something with data here
+     * }
+     * ```
+     */
+    run,
+    /** Return value of the task */
+    data,
+    /** True while the task is running */
+    isLoading,
+    /** Will receive the exception thrown by the task if one occurs */
+    error,
+    /** True if an exception has been thrown */
+    hasError
+  };
+}
+export {
+  useAsyncTask
+};
